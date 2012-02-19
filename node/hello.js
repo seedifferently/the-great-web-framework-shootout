5b26edebf6aefa31cf481b45afaf434e24332d69
Cosmin L. Neagu <cosmin@dev.ro>
2012-02-19 23:04:19 +0200

    added node.js

--- node/hello.js
+++
@@ -1,6 +0,0 @@
-var http = require('http')
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World')
-}).listen(8000)
-console.log('Server running.')

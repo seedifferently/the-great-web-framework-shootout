07da402d39c7c9758812c78ae32181f703e3aed6
Nicolas Chambrier <nchambrier@clever-age.com>
2012-02-21 12:34:59 +0100

    Added SQLite3 database file directly into repo (remove this commit to keep dynamic initialization)

--- express/express_app.js
+++ express/express_app.js
@@ -28,18 +28,6 @@ app.get('/hb_sql', function (req, res) {
   });
 });
 
-// Database initialization
-db.run("DROP TABLE lipsum", function (err) {
-  db.run("CREATE TABLE lipsum (line TEXT)", function (err) {
-    var lipsum = 'Lorem ipsum dolor sit amet, consecteteur adipiscing elit nisi ultricies. Condimentum vel, at augue nibh sed. Diam praesent metus ut eros, sem penatibus. Pellentesque. Fusce odio posuere litora non integer habitant proin. Metus accumsan nibh facilisis nostra lobortis cum diam tellus. Malesuada nostra a volutpat pede primis congue nisl feugiat in fermentum. Orci in hymenaeos. Eni tempus mi mollis lacinia orci interdum lacus. Sollicitudin aliquet, etiam. Ac. Mi, nullam ligula, tristique penatibus nisi eros nisl pede pharetra congue, aptent nulla, rhoncus tellus morbi, ornare. Magna condimentum erat turpis. Fusce arcu ve suscipit nisi phasellus rutrum a dictumst leo, laoreet dui, ultricies platea. Porta venenatis fringilla vestibulum arcu etiam condimentum non.';
-    var stmt = db.prepare("INSERT INTO lipsum VALUES (?)");
-    for (var i = 0; i < 5; i++) {
-        stmt.run((i+1) + ": " + lipsum);
-    }
-    stmt.finalize();
-  });
-});
-
 // Start server (in a ideal world, we'd wait for DB to finish its initialization, but let's make it more readable)
 app.listen(8000);
 console.log('Server running on port 8000.');

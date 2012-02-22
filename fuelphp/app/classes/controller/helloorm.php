d9c4cdd120efe6f2b9473ef4328691428c1aa090
Patrick Heller <paddy.hellergmail.com>
2012-02-22 12:12:49 +0100

    Use ORM only when needed

--- fuelphp/app/classes/controller/helloorm.php
+++ fuelphp/app/classes/controller/helloorm.php
@@ -6,6 +6,7 @@ class Controller_Helloorm extends Controller_Template {
 	
 	public function action_index()
 	{
+		Package::load('orm');	
 		$this->template->set('data', Model_Hello::find('all'), false);
 	}
 }

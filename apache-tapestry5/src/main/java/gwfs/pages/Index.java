b13bbcf484789e3c04af87418e19b9d3e00d4f23
Howard M. Lewis Ship <hlship@gmail.com>
2012-02-27 13:59:24 -0800

    Implement the static text and static template options Remove the Layout component

--- apache-tapestry5/src/main/java/gwfs/pages/Index.java
+++ apache-tapestry5/src/main/java/gwfs/pages/Index.java
@@ -1,8 +1,18 @@
 package gwfs.pages;
 
+import org.apache.tapestry5.util.TextStreamResponse;
+
 /**
  * Start page of application t5app.
  */
 public class Index {
 
+    /**
+     * This a component action event handler method, for the implicit 'action' event from the "text" ActionLink component in the template.
+     * Normally, this would perform an operation and indicate which page renders the response (which would also, normally, involve
+     * a client-side redirect); we're using a rarely-used Tapestry option to directly return a String as the response.
+     */
+    Object onActionFromText() {
+        return new TextStreamResponse("text/plain", "Hello World!");
+    }
 }

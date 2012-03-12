76d5382d8397eeb05570eef969124ff470b42d19
Seth Davis <seth@curiasolutions.com>
2012-03-11 20:54:27 -0700

    Gardening Flask Fabric test script

--- flask/__init__.py
+++ flask/__init__.py
@@ -21,7 +21,7 @@ def _average(numbers):
 
 @task
 @parallel
-def mod_wsgi(run_tests=True):
+def mod_wsgi():
     """Run the Flask mod_wsgi test."""
     INSTALL = 'build-essential apache2-utils apache2-mpm-worker python-dev ' \
               'python-pip libapache2-mod-wsgi libxml2-dev curl'
@@ -54,10 +54,6 @@ def mod_wsgi(run_tests=True):
             sudo('pip install flask==0.8')
             sudo('a2enmod wsgi')
     
-    if run_tests is False:
-        return
-    
-    with settings(hide('running', 'stdout')):
         # Setup test environment
         sudo('a2dissite 000-default')
         put(os.path.join(here, 'vhost.conf'),
@@ -158,7 +154,7 @@ def mod_wsgi(run_tests=True):
 
 @task
 @parallel
-def uwsgi(run_tests=True):
+def uwsgi():
     """Run the Flask uWSGI server test."""
     INSTALL = 'build-essential apache2-utils dtach python-dev python-pip ' \
               'libxml2-dev curl'
@@ -199,9 +195,6 @@ def uwsgi(run_tests=True):
         run('dtach -n /tmp/uwsgi -Ez uwsgi -L --http :9090 --wsgi-file '
             '/home/ubuntu/flask/flask.wsgi')
         time.sleep(1)
-        
-        if run_tests is False or run_tests == 'False':
-            return
         
         # Check the test urls
         if run('curl %s' % HELLO_TEST_URL) != "Hello World!":
@@ -292,7 +285,7 @@ def uwsgi(run_tests=True):
 
 @task
 @parallel
-def gunicorn(run_tests=True):
+def gunicorn():
     """Run the Flask Gunicorn server test."""
     INSTALL = 'build-essential apache2-utils dtach python-dev python-pip ' \
               'libxml2-dev curl'
@@ -333,9 +326,6 @@ def gunicorn(run_tests=True):
         with cd('/home/ubuntu/flask/'):
             run('dtach -n /tmp/gunicorn -Ez gunicorn -w 5 flask_app:app')
         time.sleep(1)
-        
-        if run_tests is False or run_tests == 'False':
-            return
         
         # Check the test urls
         if run('curl %s' % HELLO_TEST_URL) != "Hello World!":
@@ -422,100 +412,3 @@ def gunicorn(run_tests=True):
     env.resultsfp.seek(0)
     env.resultsfp.write(pickle.dumps(results_data))
     env.resultsfp.truncate()
-
-
-#@task
-#@parallel
-#def gunicorn(run_tests=True):
-#    """Run the Python Gunicorn (sync) server control test."""
-#    INSTALL = 'build-essential apache2-utils dtach python-dev python-pip ' \
-#              'libxml2-dev curl'
-#    TEST_URL = 'http://localhost:8000/'
-#    
-#    # Check the correct usage
-#    if not re.match(r'^test:?', sys.argv[1]):
-#        print '\nERROR: You must run "test" as the first task. Run "fab -l" ' \
-#              'for more information and a complete list of tasks.\n'
-#        sys.exit(1)
-#    
-#    # Get current instance info
-#    for instance in env.instances:
-#        if instance.public_dns_name == env.host:
-#            break
-#    
-#    with settings(hide('running', 'stdout')):
-#        if exists('/usr/sbin/ab') and exists('/usr/bin/pip') and \
-#           exists('/usr/local/bin/gunicorn') and exists('/usr/bin/dtach'):
-#            # Kill Gunicorn before running it again
-#            if int(run('ps aux | grep -c gunicorn')) > 2:
-#                sudo('killall -9 gunicorn')
-#        else:
-#            # Do installs
-#            sudo('apt-get update')
-#            sudo('apt-get -y install ' + INSTALL)
-#            sudo('pip install gunicorn==0.14.1')
-#    
-#        # Setup test environment
-#        put(here, '/home/ubuntu/', use_sudo=True)
-#        # For good measure
-#        sudo('chmod -R 777 /home/ubuntu/python/')
-#        with cd('/home/ubuntu/python/'):
-#            run('dtach -n /tmp/gunicorn -Ez gunicorn -w 5 python:application')
-#        time.sleep(1)
-#        
-#        if run_tests is False or run_tests == 'False':
-#            return
-#        
-#        # Check the test url
-#        if run('curl %s' % TEST_URL) != "Hello World!":
-#            print '*' * 80
-#            print '*%s*' % 'INVALID WEBSERVER RESPONSE'.center(78)
-#            print '*' * 80
-#            
-#            raise
-#        
-#        # Run ab
-#        output = ''
-#        for i in range(env.NUM_AB_TESTS):
-#            output += run(
-#                'ab %s %s | egrep "(^Failed)|(^Non-2xx)|(^Requests)"' %
-#                (env.AB_FLAGS, TEST_URL)
-#                )
-#            output += '\n'
-#            time.sleep(1)
-#        
-#        # Terminate instance
-#        sudo('killall -9 gunicorn')
-#    
-#    output = output.strip().split('\n')
-#    for line in output:
-#        if (line.startswith('Failed requests') or \
-#            line.startswith('Non-2xx responses')) and \
-#           not line.strip().endswith('0'):
-#            print '*' * 80
-#            print '*%s*' % 'INVALID APACHEBENCH RESPONSE'.center(78)
-#            print '*' * 80
-#            print line
-#            
-#            raise
-#        elif line.startswith('Requests per second'):
-#            env.results['hello'].append(float(re.sub('[^0-9.]', '', line)))
-#    
-#    # Get the median of all results
-#    result = _median(env.results['hello'])
-#    
-#    # Update the results file
-#    env.resultsfp.seek(0)
-#    results_data = pickle.loads(env.resultsfp.read())
-#    
-#    if env.command not in results_data:
-#        results_data[env.command] = dict()
-#    if instance.id not in results_data[env.command]:
-#        results_data[env.command][instance.id] = dict()
-#    
-#    results_data[env.command][instance.id]['hello'] = result
-#    results_data[env.command][instance.id]['tmpl'] = 'n/a'
-#    results_data[env.command][instance.id]['db'] = 'n/a'
-#    env.resultsfp.seek(0)
-#    env.resultsfp.write(pickle.dumps(results_data))
-#    env.resultsfp.truncate()

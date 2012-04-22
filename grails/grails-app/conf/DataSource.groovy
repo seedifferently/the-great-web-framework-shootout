39984d90545e5b104483f03934256749a14f0fef
Cedric G Hurst II <cedric@spantree.net>
2012-04-22 18:24:02 -0500

    adding sqlite datasource mapping and adding database file to grails project

--- grails/grails-app/conf/DataSource.groovy
+++ grails/grails-app/conf/DataSource.groovy
@@ -1,43 +1,13 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+	dbCreate = "update"
+	url = "jdbc:sqlite:hello.db"
+    driverClassName = "org.sqlite.JDBC"
+	logSql = "true"
+	dialect = "com.applerao.hibernatesqlite.dialect.SQLiteDialect"
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
-}
-// environment specific settings
-environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
-    }
 }

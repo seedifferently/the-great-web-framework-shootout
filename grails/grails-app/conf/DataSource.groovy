65032abf11e595cf3931837724b80cbe813f1b0a
Cedric G Hurst II <cedric@spantree.net>
2012-04-22 20:33:51 -0500

    enabling hibernate query cache

--- grails/grails-app/conf/DataSource.groovy
+++ grails/grails-app/conf/DataSource.groovy
@@ -8,6 +8,6 @@ dataSource {
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
+    cache.use_query_cache = true
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
